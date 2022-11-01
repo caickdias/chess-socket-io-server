ccddafcf7165a146868d74ad63be1ea215256af8
caickdias <caickandrade0@gmail.com>
2022-11-01 02:37:57 -0300

    added basic logic connection players

--- server.js
+++ server.js
@@ -1,19 +1,55 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+
 const app = express();
 const server = http.Server(app);
+const io = require('socket.io')(server);
 
 //config
 server.listen(process.env.PORT || 8001, () => {
     console.log(`[ server.js ] Listening on port ${server.address().port}`);
 });
+const users = {};
+let white = '';
+let black = '';
+let specs = [];
+// socket events
+io.on('connection', (client) => {
+    console.log(`[ server.js ] ${(client.id)} connected`);
 
-//routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/show.html'));
-})
+    client.on("username", username => {
+      const user = {
+        name: username,
+        id: client.id
+      };
+      users[client.id] = user;
+      handleUsersJoin(client.id);
+      io.emit("boardInfo", { white, black, specs});      
+      io.emit("connected", user);
+      io.emit("users", Object.values(users));
+    });
 
-app.get('/edit', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/edit.html'));
-})
+    client.on("disconnect", () => {
+      handleUserDisconnect(client.id);
+      io.emit("boardInfo", { white, black, specs});      
+      delete users[client.id];
+      io.emit("disconnected", client.id);
+    })
+
+});
+
+const handleUserDisconnect = id => {
+  if(white === id) white = '';
+  else if(black === id) black = '';
+  else specs = specs.filter(specID => specID !== id);      
+}
+
+const handleUsersJoin = (id) => {
+  const hasWhitePlayer = white.length > 0;
+  const hasBlackPlayer = black.length > 0;
+  
+  if(!hasWhitePlayer) white = id;
+  else if (!hasBlackPlayer) black = id;
+  else specs.push(id);
+}
